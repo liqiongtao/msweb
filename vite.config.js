55f14b0ac6493fdec2572f045f0b5e3e4b7bcec8
liqiongtao <hnatao@126.com>
2023-06-11 17:32:53 +0800

    bufix

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import vueSetupExend from 'vite-plugin-vue-setup-extend'
@@ -7,57 +7,60 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import AutoImport from 'unplugin-auto-import/vite'
 
-export default defineConfig({
-    base: './',
-    plugins: [
-        vue(),
-        vueSetupExend(),
-        AutoImport({
-            resolvers: [ElementPlusResolver()],
-            imports: ['vue', 'vue-router', 'vuex'],
-            eslintrc: {
-                enabled: false,
-                globalsPropValue: true,
-                filepath: './.eslintrc-auto-import.json'
+export default ({ mode }) => {
+    const env = loadEnv(mode, process.cwd())
+    return defineConfig({
+        base: env.VITE_ROUTER_BASE,
+        plugins: [
+            vue(),
+            vueSetupExend(),
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+                imports: ['vue', 'vue-router', 'vuex'],
+                eslintrc: {
+                    enabled: false,
+                    globalsPropValue: true,
+                    filepath: './.eslintrc-auto-import.json'
+                }
+            }),
+            Components({
+                resolvers: [ElementPlusResolver({ importStyle: 'sass' })]
+            }),
+            ElementPlus()
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src')
             }
-        }),
-        Components({
-            resolvers: [ElementPlusResolver({ importStyle: 'sass' })]
-        }),
-        ElementPlus()
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: `
                     @use "@/assets/element.scss" as *;
                     @use "@/assets/element-dark.scss";
                 `
-            }
-        }
-    },
-    build: {
-        target: ['es2015'],
-        outDir: 'dist',
-        manifest: true,
-        sourcemap: false,
-        emptyOutDir: true,
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    lodash: ['lodash']
                 }
             }
+        },
+        build: {
+            target: ['es2015'],
+            outDir: 'dist',
+            manifest: true,
+            sourcemap: false,
+            emptyOutDir: true,
+            rollupOptions: {
+                output: {
+                    manualChunks: {
+                        lodash: ['lodash']
+                    }
+                }
+            }
+        },
+        server: {
+            open: true,
+            port: 3301,
+            hmr: { overlay: false }
         }
-    },
-    server: {
-        open: true,
-        port: 3301,
-        hmr: { overlay: false }
-    }
-})
+    })
+}
